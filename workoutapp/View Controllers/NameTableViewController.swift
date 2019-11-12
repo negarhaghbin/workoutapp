e3def6e6c56f41e77e0db57aaa1526f41695ca02
Negar <nbeanm@gmail.com>
2019-11-12 12:44:50 -0500

    edit name fixed

--- workoutapp/View Controllers/NameTableViewController.swift
+++ workoutapp/View Controllers/NameTableViewController.swift
@@ -13,103 +13,26 @@ class NameTableViewController: UITableViewController {
     
     var user : User = User()
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? NameTableViewCell else {
-            return NameTableViewCell()
-        }
-        
-        cell.name.text = user.name
-
-        return cell
+        textField?.text=user.name
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row==0{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as? NameTableViewCell
-            
-            user.name = (cell?.name!.text)!
-            //#########Something is wrong here
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            textField.becomeFirstResponder()
         }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     @IBAction func changeName(_ sender: Any) {
-        let realm = try! Realm()
-        let userModels = realm.objects(UserModel.self)
-        for userModel in userModels{
-            if userModel.uuid == UserDefaults.standard.string(forKey: "uuid"){
-                try! realm.write {
-                    userModel.name=user.name
-                }
-                UserDefaults.standard.set(userModel.uuid, forKey: "uuid")
-            }
+        let realm=try! Realm()
+        let specificPerson = realm.object(ofType: UserModel.self, forPrimaryKey: UserDefaults.standard.string(forKey: "uuid"))
+        try! realm.write {
+            specificPerson?.name=(textField.text)!
         }
+         _ = navigationController?.popViewController(animated: true)
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
